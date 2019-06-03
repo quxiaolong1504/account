aa485f2c532ab95d2832b7493550985af8756186
quxiaolong <quxiaolong@zhihu.com>
2019-06-03 17:15:48 +0800

    项目基础 done

---
+++ cmd/web/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/quxiaolong/account/pkg/rpcs/seqd"
+)
+
+func main() {
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		id := seqd.Generate()
+		c.JSON(200, gin.H{
+			"message": fmt.Sprintf("pong  id:%d node: %d id:%d", id, id.Node(), id.Time()),
+		})
+	})
+	r.Run()
+}
