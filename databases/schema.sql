210b4f488ca654a9b0157fdc6b2556e63f1a540f
quxiaolong <quxiaolong@zhihu.com>
2019-06-06 16:37:29 +0800

    更新下 schema.sql

--- databases/schema.sql
+++ databases/schema.sql
@@ -11,3 +11,30 @@ CREATE TABLE `user` (
   UNIQUE KEY `uin_uid` (`uid`),
   KEY `uin_phone` (`phone`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `user_wechat_ship` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` varchar(50) NOT NULL DEFAULT '',
+  `unionid` varchar(50) NOT NULL DEFAULT '',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uni_uid` (`uid`,`unionid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `wechat_info` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `unionid` varchar(100) NOT NULL DEFAULT '',
+  `openid` varchar(100) NOT NULL DEFAULT '',
+  `access_token` varchar(100) NOT NULL DEFAULT '',
+  `refresh_token` varchar(100) NOT NULL DEFAULT '',
+  `name` varchar(100) NOT NULL DEFAULT '',
+  `gender` tinyint(1) NOT NULL,
+  `province` varchar(20) NOT NULL DEFAULT '',
+  `city` varchar(20) NOT NULL DEFAULT '',
+  `country` varchar(50) NOT NULL DEFAULT '',
+  `avatar` varchar(50) NOT NULL DEFAULT '',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
