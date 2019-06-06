68d7d08805e01dfce18577ed0bd7220a329df488
quxiaolong <quxiaolong@zhihu.com>
2019-06-06 13:28:30 +0800

    增加打点配置

--- pkg/web/middlewares/monitor.go
+++ pkg/web/middlewares/monitor.go
@@ -5,11 +5,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/influxdata/influxdb1-client/v2"
 	"github.com/quxiaolong/account/pkg/utils/storage"
+	"net/http"
 	"strings"
+	"time"
 )
 
 func Monitor() gin.HandlerFunc {
 	fn := func(c *gin.Context) {
+		start := time.Now()
 		// 打 qps 点
 		go func() {
 			batchPoint,_ := client.NewBatchPoints(client.BatchPointsConfig{
@@ -18,35 +21,61 @@ func Monitor() gin.HandlerFunc {
 			})
 			args := strings.Split(c.HandlerName(), ".")
 			name := fmt.Sprintf("%s_%s", args[len(args)-1], c.Request.Method)
-			fmt.Print()
-			point1, _ := client.NewPoint(
-				"qps",
-				map[string]string{"handler": name, "method": c.Request.Method},
-				map[string]interface{} {
-					"value": 1,
-				},)
 
-			if c.Writer.Status() >= 500 || c.Writer.Status() < 600 {
-				p5xx, _ := client.NewPoint(
-					"5xx",
-					map[string]string{"handler": name, "method": c.Request.Method},
-					map[string]interface{} {
-						"value": 1,
-					},
-				)
-				batchPoint.AddPoint(p5xx)
+			// performance point
+			point := genPerformancePoint(name, start,  c)
+
+			// status point
+			statusPoint := genStatusPoint(name, c)
+			if statusPoint != nil {
+				batchPoint.AddPoint(statusPoint)
 			}
 
-
-			batchPoint.AddPoint(point1)
+			batchPoint.AddPoint(point)
 			_ = storage.InfluxDB.Write(batchPoint)
 		}()
 
-		// TODO: 打请求耗时的点 & status code
 
 
 		c.Next()
 	}
 
 	return gin.HandlerFunc(fn)
+}
+
+
+func genPerformancePoint(handlerName string, start time.Time, c *gin.Context) *client.Point {
+	time.Sleep(time.Second * 2)
+	rt := time.Since(start).Seconds()
+	point1, _ := client.NewPoint(
+		"performance",
+		map[string]string{"handler": handlerName, "method": c.Request.Method},
+		map[string]interface{} {
+			"qps": 1,
+			"rt": rt,
+		},)
+	return point1
+}
+
+func genStatusPoint(name string, c *gin.Context) *client.Point{
+	if c.Writer.Status() >= http.StatusUnauthorized && c.Writer.Status() < http.StatusNetworkAuthenticationRequired {
+		_5xx := 0
+		_401 := 0
+		if c.Writer.Status() >= http.StatusInternalServerError && c.Writer.Status() <= http.StatusNetworkAuthenticationRequired{
+			_5xx = 1
+		}
+		if c.Writer.Status() == http.StatusUnauthorized {
+			_401 = 1
+		}
+		point, _ := client.NewPoint(
+			"status",
+			map[string]string{"handler": name, "method": c.Request.Method},
+			map[string]interface{} {
+				"5xx": _5xx,
+				"401": _401,
+			},
+		)
+		return  point
+	}
+	return nil
 }
